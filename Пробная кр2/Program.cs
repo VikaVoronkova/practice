1030af49627da8bb023667465f192c8d24493dc9
VikaVoronkova <voronkovavika19@gmail.com>
2026-03-29 19:31:53 +0300

    Add files via upload

---
+++ Пробная кр2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1.Employee emp1 = new Task1.Employee("Вадим", "Вадимыч", 48, 60000.5);
+            
+            Console.WriteLine("Имя: " + emp1.Name);
+            Console.WriteLine("Фамилия: " + emp1.Surname);
+            Console.WriteLine("Возраст: " + emp1.Age);
+            Console.WriteLine("ЗП: " + emp1.Salary);
+            Console.WriteLine("ID: " + emp1.ID);
+
+            emp1.CelebrateBirthday();
+            Console.WriteLine("Возраст: " + emp1.Age);
+
+            emp1.Promote(30);
+            Console.WriteLine("ЗП: " + emp1.Salary);
+
+            Console.WriteLine(emp1.GetPerspective());
+        }
+    }
+}
---
+++ Пробная кр2/Task1.cs
@@ -0,0 +1,52 @@
+﻿using CW5.Variant03;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    public class Task1
+    {
+        public struct Employee : IEmployee
+        {
+            //pole
+            private string _name;
+            private string _surname;
+            private int _age;
+            private double _salary;
+            private int _id;
+            private int _count = 0;
+            //svoistvo
+            public string Name => _name;
+            public string Surname => _surname;
+            public int Age => _age;
+            public double Salary => _salary;
+            public int ID => _id;
+            //konstruktor
+            public Employee(string name, string surname, int age, double salary)
+            {
+                _name = name;
+                _surname = surname;
+                _age = age;
+                _salary = salary;
+                _count++;
+                _id = 100000 + _count;
+            }
+            //method
+            public void CelebrateBirthday()
+            {
+                _age++;
+            }
+            public void Promote(double value)
+            {
+                _salary += value;
+            }
+            public double GetPerspective()
+            {
+                return Math.Sqrt(100 - Age) * Salary * 1.05;
+            }
+        }
+    }
+}
